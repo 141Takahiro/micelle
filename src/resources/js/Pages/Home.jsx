330c4fa8a39c82009865aa6c1b968f44ea1f0a28
takahiro ishii <paperback63@gmail.com>
2025-05-31 21:19:52 +0900

    テスト確認用です

--- src/resources/js/Pages/Home.jsx
+++ src/resources/js/Pages/Home.jsx
@@ -13,7 +13,6 @@ import cameraAdd from "../assets/icons/camera-add.png";
 import folderOpen from "../assets/icons/folder-open.png";
 import { usePage } from "@inertiajs/react";
 
-
 export default function Home({ rooms = [] }) {
 
     const defaultImage = "/storage/images/default-image.png";
@@ -361,6 +360,7 @@ export default function Home({ rooms = [] }) {
 
             <div className="md:flex md:flex-row">
                 <div className="basis-1/3 border-2 border-solid rounded-sm md:m-2 shadow-xl justify-items-center ">
+                <p>テスト用。後で消します。</p>
                     <h2 className="text-xl font-bold mb-2">今週のタスク</h2>
 
                     {rooms.length === 0 ? (
